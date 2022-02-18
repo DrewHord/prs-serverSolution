fafab1a0d4f81abc68699df449da1b2f63963639
Andrew Hord <drewhord@gmail.com>
2022-02-18 14:06:41 -0500

    fixed startup

--- prs-server/Startup.cs
+++ prs-server/Startup.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using prs_server.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,25 +23,23 @@ namespace prs_server {
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
-
-            services.AddControllers();
-            services.AddSwaggerGen(c => {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "prs_server", Version = "v1" });
+            services.AddDbContext<AppDbContext>(x => {
+                var connStr = Configuration.GetConnectionString("AppDbContext");
+                x.UseSqlServer(connStr);
             });
+            services.AddControllers();
+            services.AddCors();
         }
+        
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
             if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "prs_server v1"));
-            }
-
+            }               
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
             app.UseRouting();
-
             app.UseAuthorization();
-
             app.UseEndpoints(endpoints => {
                 endpoints.MapControllers();
             });
